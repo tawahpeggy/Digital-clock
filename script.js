0107d686fe3e72ecf81bfd4a418bdf3a9e53b2ff
tawahpeggy <tawahpeggy@gmail.com>
2022-03-14 19:01:50 +0100

    convert  minutes, seconds and hours to string. Then append 0 on each when value is less than 2

--- script.js
+++ script.js
@@ -1,8 +1,23 @@
 function digitalClock() {
     var date = new Date;
-    var hours = date.getHours();
-    var minutes = date.getMinutes();
-    var seconds = date.getSeconds();
-    var day = date.getDay();
-    
+    var hours = date.getHours() + ''; //the ' ' converts these numbers to strings
+    var minutes = date.getMinutes() + '';
+    var seconds = date.getSeconds() + '';
+    var day = date.getDay() + '';
+
+    // append 0 if value is one digit
+    if (hours.length < 2) {
+        hours = '0' + hours;
+    }
+
+    if (minutes.length < 2) {
+        minutes = '0' + minutes;
+    }
+
+    if (seconds.length < 2) {
+        seconds = '0' + seconds;
+    }
+var clock = hours + ':' + minutes + ':' + seconds;
+console.log(clock);
 }
+digitalClock();
